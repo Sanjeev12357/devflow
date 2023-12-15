7ed49d8078b811865ee2327b69bc37edb354a839
sanjeev saini <sanjeevsinghsaini48@gmail.com>
2023-12-15 13:34:40 +0530

    latest commit2

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,21 +1,22 @@
 import Question from "@/components/forms/Question";
-import { getUserById } from "@/lib/actions/user.action";
+// import { getUserById } from "@/lib/actions/user.action";
 // import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 import React from "react";
 
 const page = async () => {
   // const { userId } = auth();
-  const userId = "CL123";
-  if (!userId) redirect("/sign-in");
+  // const userId = "CL123";
+  // if (!userId) redirect("/sign-in");
 
-  const mongoUser = await getUserById({ userId });
-  console.log(mongoUser);
+  // const mongoUser = await getUserById({ userId });
+  // console.log(mongoUser);
+  // mongoUserId={JSON.stringify(mongoUser._id)}
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
       <div>
-        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+        <Question />
       </div>
     </div>
   );
