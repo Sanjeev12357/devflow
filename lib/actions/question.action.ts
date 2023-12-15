239dd2590f7ab96d3db2e4b283ca3ded421a9370
sanjeev saini <sanjeevsinghsaini48@gmail.com>
2023-12-15 13:07:49 +0530

    1

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -9,12 +9,12 @@ import { revalidatePath } from "next/cache";
 
 export async function getQuestions(params: GetQuestionsParams) {
   try {
-    connectToDatabase();
+    await connectToDatabase();
 
     const questions = await Question.find({})
       .populate({ path: "tags", model: Tag })
       .populate({ path: "author", model: User })
-      .sort({createdAt:-1});
+      .sort({ createdAt: -1 });
     return { questions };
   } catch (error) {
     console.log(error);
