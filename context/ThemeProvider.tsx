bd3c0d087b28568316dafd38dee0fe3d621846e6
sanjeev saini <sanjeevsinghsaini48@gmail.com>
2024-02-05 14:15:15 +0530

    added navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,47 +1,45 @@
+// Import statements
 "use client"
+import React, { createContext, useContext, useEffect, useState } from "react";
 
-import React,{createContext,useContext,useState,useEffect} from 'react';
-
-interface ThemeContextType{
-    mode :string;
-    setMode:(mode:string)=>void;
+interface ThemeContextType {
+    mode: string;
+    setMode: (mode: string) => void;
 }
 
-const ThemeContext=createContext<ThemeContextType | undefined>(undefined);
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
-export function ThemeProvider({children}:{
-    children:React.ReactNode
-}){
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const [mode, setMode] = useState('');
 
-    const [mode,setMode]=useState('');
+    const handleThemeChange = () => {
+       if(localStorage.theme === 'dark' || (!("theme" in localStorage) &&
+       window.matchMedia("(prefers-color-scheme:dark)").matches)
+       ){
+        setMode('dark');
+       document.documentElement.classList.add('dark');
+       }else{
+        setMode('light');
+        document.documentElement.classList.remove('dark');
+       }
+    };
 
-    const handleThemeChange=()=>{
-        if(mode === 'dark'){
-            setMode('light');
-            document.documentElement.classList.add('light');
-        }else{
-            setMode('dark');
-            document.documentElement.classList.add('dark');
-        }
-    }
-
-    useEffect(()=>{
+    useEffect(() => {
         handleThemeChange();
-    },[mode]);
+    }, [mode]); // Empty dependency array to run the effect only on mount
 
     return (
-        <ThemeContext.Provider value={{mode,setMode}}>
+        <ThemeContext.Provider value={{ mode, setMode }}>
             {children}
         </ThemeContext.Provider>
-    )
+    );
 }
 
-export function useTheme(){
+export function useTheme() {
+    const context = useContext(ThemeContext);
 
-    const context=useContext(ThemeContext);
-
-    if(context=== undefined){
-        throw new Error('usethem must be used within a themeprovider');
+    if (context === undefined) {
+        throw new Error('useTheme must be used within a ThemeProvider');
     }
 
     return context;
